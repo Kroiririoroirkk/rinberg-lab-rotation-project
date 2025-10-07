837dccbaa27e5655473f017d8615a0121d10f570
Eric Tao <taoeric9@gmail.com>
2025-10-07 14:36:44 -0400

    Add dF/F video and better spatial/temporal blurring

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ from tkinter.font import Font
 from typing import Final, Self
 
 import h5py
+import matplotlib
 import numpy as np
 import read_roi
+import scipy.ndimage
 import tifffile
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
@@ -34,16 +36,25 @@ TIF_FOLDER: Final[Path] = Path('data/GLOM/TIFFs')
 H5_PATH: Final[Path] = Path(
     'data/GLOM/mouse0953_sess20_D2025_8_18T12_36_43.h5')
 H5_PICKLE_PATH: Final[Path] = Path('metadata.pkl')
-FRAMES_AVG_START: Final[int] = 0
-FRAMES_AVG_END: Final[int] = 100
+NUM_FRAMES_BUYIN: Final[int] = 5
+NUM_FRAMES_AVG: Final[int] = 30
 BASELINE_AVG_START: Final[int] = 0
-BASELINE_AVG_END: Final[int] = 100
-DEFAULT_WINDOW_SIZE: Final[str] = '1600x850'
-SMALL_PAD: int = 10
-MEDIUM_PAD: int = 20
-LARGE_PAD: int = 30
-HEADING_FONT: Font = ('', 18)
-SMOOTHING_DISTANCE: int = 5
+BASELINE_AVG_END: Final[int] = 50
+SLOW_SPEED: Final[float] = 0.2
+DEFAULT_WINDOW_WIDTH: Final[int] = 1600
+DEFAULT_WINDOW_HEIGHT: Final[int] = 850
+DEFAULT_WINDOW_SIZE: Final[
+    str] = f'{DEFAULT_WINDOW_WIDTH}x{DEFAULT_WINDOW_HEIGHT}'
+SMALL_PAD: Final[int] = 10
+MEDIUM_PAD: Final[int] = 20
+LARGE_PAD: Final[int] = 30
+HEADING_FONT: Final[Font] = ('', 18)
+DEFAULT_SPATIAL_BLUR: Final[float] = 1.0
+DEFAULT_TEMPORAL_BLUR: Final[int] = 5
+MIN_DELTA_F: Final[float] = -0.5
+MAX_DELTA_F: Final[float] = 2.0
+HEAT_CMAP: Final[matplotlib.colors.Colormap] = matplotlib.colormaps['inferno']
+JOB_SCHEDULE_DELAY: Final[int] = 1000
 
 
 class Odor(Enum):
@@ -191,8 +202,8 @@ def load_rois() -> OrderedDict[ROIName, ROI]:
 
 
 def load_caption(tif_path: Path, trial_metadata: TrialMetadata) -> str:
-    return (f'Average of frames {FRAMES_AVG_START}:'
-            f'{FRAMES_AVG_END} ({tif_path.name})\nOdors = '
+    return (f'Average of {NUM_FRAMES_AVG} frames after odor presentation '
+            f'({tif_path.name})\nOdors = '
             f'{trial_metadata.odor1_flow}% {trial_metadata.odor1.value}, '
             f'{trial_metadata.odor2_flow}% {trial_metadata.odor2.value}\n'
             f'Target = {trial_metadata.target.value}, '
@@ -205,17 +216,22 @@ def load_image(tif_path: Path) -> NDArray[np.int16]:
     return tiff_arr
 
 
-def render_frame(tiff_arr: NDArray[np.int16], rois: OrderedDict[ROIName, ROI],
-                 rois_focused: list[ROIName], frame: int,
-                 smoothen: bool) -> Image:
-    if smoothen:
-        tiff_image = Image.fromarray(
-            np.mean(tiff_arr[frame:frame + SMOOTHING_DISTANCE], axis=0))
-    else:
-        tiff_image = Image.fromarray(tiff_arr[frame])
-    tiff_image_rgb = Image.new('RGB', tiff_image.size)
-    tiff_image_rgb.paste(tiff_image)
-    draw = ImageDraw.Draw(tiff_image_rgb)
+def make_delta_f_image(tiff_arr: NDArray[float],
+                       median_tiff_arr: NDArray[float],
+                       threshold: int = 100) -> NDArray[np.uint8]:
+    tiff_arr_c = tiff_arr.copy()
+    tiff_arr_c[median_tiff_arr < threshold] = MIN_DELTA_F
+    mask = (median_tiff_arr >= threshold)
+    tiff_arr_c[mask] = (tiff_arr_c[mask] -
+                        median_tiff_arr[mask]) / median_tiff_arr[mask]
+    tiff_arr_c = np.clip(tiff_arr_c, MIN_DELTA_F, MAX_DELTA_F)
+    tiff_arr_c = (tiff_arr_c - MIN_DELTA_F) / (MAX_DELTA_F - MIN_DELTA_F)
+    tiff_arr_c = HEAT_CMAP(tiff_arr_c, bytes=True)
+    return tiff_arr_c
+
+
+def draw_rois(draw: ImageDraw.Draw, rois: OrderedDict[ROIName, ROI],
+              rois_focused: list[ROIName]) -> None:
     for roi_name, roi in rois.items():
         x0, y0 = roi['left'], roi['top']
         x1, y1 = x0 + roi['width'], y0 + roi['height']
@@ -229,6 +245,23 @@ def render_frame(tiff_arr: NDArray[np.int16], rois: OrderedDict[ROIName, ROI],
                       font_size=24)
         else:
             draw.ellipse([x0, y0, x1, y1], outline='blue', width=3)
+
+
+def render_frame(tiff_arr: NDArray[np.int16], rois: OrderedDict[ROIName, ROI],
+                 rois_focused: list[ROIName], hide_rois: bool, frame: int,
+                 spatial_blur: int, temporal_blur: int,
+                 median_tiff_arr: NDArray[np.int16] | None) -> Image:
+    tiff_arr_processed = np.mean(tiff_arr[frame:frame + temporal_blur + 1],
+                                 axis=0)
+    tiff_arr_processed = scipy.ndimage.gaussian_filter(tiff_arr_processed,
+                                                       sigma=spatial_blur)
+    if median_tiff_arr is not None:
+        tiff_arr_processed = make_delta_f_image(tiff_arr_processed,
+                                                median_tiff_arr)
+    tiff_image = Image.fromarray(tiff_arr_processed)
+    tiff_image_rgb = Image.new('RGB', tiff_image.size)
+    tiff_image_rgb.paste(tiff_image)
+    draw = ImageDraw.Draw(tiff_image_rgb)
     x0, y0 = tiff_image.width - 1, 0
     draw.rectangle([x0 - 100, y0, x0, y0 + 30], fill=(0, 0, 0, 10))
     draw.text([x0 - 5, y0 + 5],
@@ -236,30 +269,32 @@ def render_frame(tiff_arr: NDArray[np.int16], rois: OrderedDict[ROIName, ROI],
               fill='red',
               font_size=24,
               anchor='rt')
+    if not hide_rois:
+        draw_rois(draw, rois, rois_focused)
     return tiff_image_rgb
 
 
-def render_thumbnail(tiff_arr: NDArray[np.int16], rois: OrderedDict[ROIName,
-                                                                    ROI],
-                     rois_focused: list[ROIName]) -> Image:
-    tiff_arr_avg = np.mean(tiff_arr[FRAMES_AVG_START:FRAMES_AVG_END], axis=0)
-    tiff_image = Image.fromarray(tiff_arr_avg)
+def render_thumbnail(tiff_arr: NDArray[np.int16], metadata: TrialMetadata,
+                     rois: OrderedDict[ROIName,
+                                       ROI], rois_focused: list[ROIName],
+                     hide_rois: bool, spatial_blur: int,
+                     median_tiff_arr: NDArray[np.int16] | None) -> Image:
+    start_frame = np.argmax(
+        metadata.frame_times > metadata.odor_time) + NUM_FRAMES_BUYIN
+    tiff_arr_processed = np.mean(tiff_arr[start_frame:start_frame +
+                                          NUM_FRAMES_AVG],
+                                 axis=0)
+    tiff_arr_processed = scipy.ndimage.gaussian_filter(tiff_arr_processed,
+                                                       sigma=spatial_blur)
+    if median_tiff_arr is not None:
+        tiff_arr_processed = make_delta_f_image(tiff_arr_processed,
+                                                median_tiff_arr)
+    tiff_image = Image.fromarray(tiff_arr_processed)
     tiff_image_rgb = Image.new('RGB', tiff_image.size)
     tiff_image_rgb.paste(tiff_image)
-    draw = ImageDraw.Draw(tiff_image_rgb)
-    for roi_name, roi in rois.items():
-        x0, y0 = roi['left'], roi['top']
-        x1, y1 = x0 + roi['width'], y0 + roi['height']
-        if roi_name in rois_focused:
-            draw.ellipse([x0, y0, x1, y1], outline='red', width=3)
-            draw.rectangle([x0 - 10, y0 - 10, x0 + 10, y0 + 16],
-                           fill=(0, 0, 0, 10))
-            draw.text([x0 - 10, y0 - 10],
-                      str(rois_focused.index(roi_name) + 1),
-                      fill='red',
-                      font_size=24)
-        else:
-            draw.ellipse([x0, y0, x1, y1], outline='blue', width=3)
+    if not hide_rois:
+        draw = ImageDraw.Draw(tiff_image_rgb)
+        draw_rois(draw, rois, rois_focused)
     return tiff_image_rgb
 
 
@@ -392,13 +427,17 @@ def run_gui() -> None:
     _tif_file_i = 0
     tif_path = tif_files[_tif_file_i]
     tiff_arr = load_image(tif_path)
+    _median_tiff_arr = None
     metadata = h5_data[_tif_file_i]
     tk_image_job = None
-    resize_job = None
+    _update_figure = False
+    _update_job = None
     rois = load_rois()
     rois_focused = []
-    plot_setting = PlotSetting.BEHAVIOR
+    plot_setting = PlotSetting.NONE
     image = None  # prevent garbage collection of image
+    _window_width = 0
+    _window_height = 0
 
     root = tk.Tk()
     root.title('Calcium imaging analyzer')
@@ -449,17 +488,44 @@ def run_gui() -> None:
 
     button2_frame = ttk.Frame(ci_frame)
     button2_frame.pack(pady=(0, SMALL_PAD))
-    start_from_odor_var = tk.BooleanVar()
+    start_from_odor_var = tk.BooleanVar(value=True)
     start_from_odor_button = ttk.Checkbutton(
         button2_frame,
         text='Play from odor presentation?',
         variable=start_from_odor_var)
     start_from_odor_button.pack(side='left', padx=(0, SMALL_PAD))
-    smoothen_video_var = tk.BooleanVar()
-    smoothen_video_button = ttk.Checkbutton(button2_frame,
-                                            text='Smoothen video?',
-                                            variable=smoothen_video_var)
-    smoothen_video_button.pack(side='left')
+    plot_delta_var = tk.BooleanVar(value=True)
+    plot_delta_button = ttk.Checkbutton(button2_frame,
+                                        text='Plot ΔF/F?',
+                                        variable=plot_delta_var)
+    plot_delta_button.pack(side='left', padx=(0, SMALL_PAD))
+    hide_rois_var = tk.BooleanVar()
+    hide_rois_button = ttk.Checkbutton(button2_frame,
+                                       text='Hide ROIs?',
+                                       variable=hide_rois_var)
+    hide_rois_button.pack(side='left')
+
+    button3_frame = ttk.Frame(ci_frame)
+    button3_frame.pack()
+    spatial_blur_label = ttk.Label(button3_frame, text='Spatial blur:')
+    spatial_blur_label.pack(side='left', padx=(0, SMALL_PAD))
+    spatial_blur_var = tk.DoubleVar(value=DEFAULT_SPATIAL_BLUR)
+    spatial_blur_slider = tk.Scale(button3_frame,
+                                   from_=0,
+                                   to=3,
+                                   resolution=0.1,
+                                   orient='horizontal',
+                                   variable=spatial_blur_var)
+    spatial_blur_slider.pack(side='left', padx=(0, SMALL_PAD))
+    temporal_blur_label = ttk.Label(button3_frame, text='Temporal blur:')
+    temporal_blur_label.pack(side='left', padx=(0, SMALL_PAD))
+    temporal_blur_var = tk.IntVar(value=DEFAULT_TEMPORAL_BLUR)
+    temporal_blur_slider = tk.Scale(button3_frame,
+                                    from_=0,
+                                    to=10,
+                                    orient='horizontal',
+                                    variable=temporal_blur_var)
+    temporal_blur_slider.pack(side='left', padx=(0, SMALL_PAD))
 
     side_frame = ttk.Frame(root, style='Invisible.TFrame')
     side_frame.pack(padx=(0, LARGE_PAD), side='right', fill='y', expand=True)
@@ -487,11 +553,11 @@ def run_gui() -> None:
         button.pack(padx=int(SMALL_PAD / 2), side='left')
         plot_buttons.append(button)
 
-    def update_job(job: str) -> None:
+    def set_image_job(job: str) -> None:
         nonlocal tk_image_job
         tk_image_job = job
 
-    def stop_job() -> None:
+    def stop_image_job() -> None:
         global cache_line_behavioral_plot, cache_line_fluorescence_plot
         nonlocal tk_image_job
         if tk_image_job:
@@ -504,15 +570,19 @@ def run_gui() -> None:
             cache_line_fluorescence_plot.remove()
             cache_line_fluorescence_plot = None
 
-    def update_resize_job(job: str) -> None:
-        nonlocal resize_job
-        resize_job = job
+    def cue_update_job(update_figure: bool = True) -> None:
+        nonlocal _update_figure, _update_job
 
-    def stop_resize_job() -> None:
-        nonlocal resize_job
-        if resize_job:
-            root.after_cancel(resize_job)
-            resize_job = None
+        def _update():
+            nonlocal _update_figure, _update_job
+            update(redraw_figure=_update_figure)
+            _update_job = None
+            _update_figure = False
+
+        if _update_job:
+            root.after_cancel(_update_job)
+        _update_figure = _update_figure or update_figure
+        _update_job = root.after(JOB_SCHEDULE_DELAY, _update)
 
     def get_tif_file_i() -> int:
         return _tif_file_i
@@ -523,7 +593,14 @@ def run_gui() -> None:
         tif_path = tif_files[_tif_file_i]
         tiff_arr = load_image(tif_path)
         metadata = h5_data[_tif_file_i]
-        stop_job()
+        stop_image_job()
+
+    def get_median_tiff_arr() -> NDArray[np.int16]:
+        nonlocal _median_tiff_arr
+        if _median_tiff_arr is None:
+            _median_tiff_arr = np.median(
+                tiff_arr[BASELINE_AVG_START:BASELINE_AVG_END], axis=0)
+        return _median_tiff_arr
 
     def set_rois_focused(r: list[ROIName]) -> None:
         nonlocal rois_focused
@@ -541,18 +618,33 @@ def run_gui() -> None:
         tk_image.delete('IMG')
         tk_image.create_image(0, 0, image=image, anchor='nw', tags='IMG')
 
+    def was_window_resized(width: int, height: int) -> bool:
+        return _window_width != width or _window_height != height
+
+    def set_window_size(width: int, height: int) -> None:
+        nonlocal _window_width, _window_height
+        _window_width = width
+        _window_height = height
+
     def update(redraw_image: bool = True,
                redraw_figure: bool = True,
-               frame: int | None = None,
-               smoothen: bool = False) -> None:
+               frame: int | None = None) -> None:
         if redraw_image:
             ci_label.configure(text=load_caption(tif_path, metadata))
+            hide_rois = hide_rois_var.get()
+            spatial_blur = spatial_blur_var.get()
+            temporal_blur = temporal_blur_var.get()
+            median_tiff_arr = get_median_tiff_arr() if plot_delta_var.get(
+            ) else None
             if frame is None:
-                set_ci_image(render_thumbnail(tiff_arr, rois, rois_focused))
+                set_ci_image(
+                    render_thumbnail(tiff_arr, metadata, rois, rois_focused,
+                                     hide_rois, spatial_blur, median_tiff_arr))
             else:
                 set_ci_image(
-                    render_frame(tiff_arr, rois, rois_focused, frame,
-                                 smoothen))
+                    render_frame(tiff_arr, rois, rois_focused, hide_rois,
+                                 frame, spatial_blur, temporal_blur,
+                                 median_tiff_arr))
         if redraw_figure:
             render_plot(fig, plot_setting, metadata, tiff_arr, rois,
                         rois_focused, frame)
@@ -569,33 +661,32 @@ def run_gui() -> None:
 
     def on_play_button(speed: float) -> None:
 
-        def render(i: int, smoothen: bool) -> None:
+        def render(i: int) -> None:
             if i + 1 < tiff_arr.shape[0]:
-                update(frame=i, smoothen=smoothen)
+                update(frame=i)
                 dt = int(
                     (metadata.frame_times[i + 1] - metadata.frame_times[i]) /
                     speed)
-                update_job(
-                    tk_image.after(dt if dt > 0 else 0,
-                                   lambda: render(i + 1, smoothen)))
+                set_image_job(
+                    tk_image.after(dt if dt > 0 else 0, lambda: render(i + 1)))
             else:
-                stop_job()
+                stop_image_job()
                 update()
 
-        stop_job()
+        stop_image_job()
 
         if start_from_odor_var.get():
             i = np.argmax(metadata.frame_times > metadata.odor_time)
-            render(i - 5 if i > 5 else 0, smoothen_video_var.get())
+            render(i - 5 if i > 5 else 0)
         else:
-            render(0, smoothen_video_var.get())
+            render(0)
 
     def on_stop_button() -> None:
-        stop_job()
+        stop_image_job()
         update()
 
     def on_select_all_button() -> None:
-        stop_job()
+        stop_image_job()
         set_rois_focused(list(rois.keys()))
         update()
 
@@ -608,7 +699,7 @@ def run_gui() -> None:
             pass
 
     def on_roi_click(roi_name: ROIName | None, shift_pressed: bool) -> None:
-        stop_job()
+        stop_image_job()
         if shift_pressed:
             if roi_name:
                 if roi_name in rois_focused:
@@ -632,7 +723,7 @@ def run_gui() -> None:
         on_roi_click(roi_name, shift_pressed)
 
     def on_plot_setting_button() -> None:
-        stop_job()
+        stop_image_job()
         set_plot_setting(PlotSetting(plot_var.get()))
         update()
 
@@ -640,22 +731,31 @@ def run_gui() -> None:
         if event.widget == root:
             new_width = event.width
             new_height = event.height
-            ci_image_size = int(0.6 * min(new_width, new_height))
-            tk_image.configure(width=ci_image_size, height=ci_image_size)
-            plot_height = int(0.6 * new_height)
-            plot_width = int(1.4 * plot_height)
-            canvas.get_tk_widget().configure(width=plot_width,
-                                             height=plot_height)
-            stop_resize_job()
-            update_resize_job(root.after(1000, update))
+            if was_window_resized(new_width, new_height):
+                set_window_size(new_width, new_height)
+                ci_image_size = int(0.6 * min(new_width, new_height))
+                tk_image.configure(width=ci_image_size, height=ci_image_size)
+                plot_height = int(0.6 * new_height)
+                plot_width = int(1.4 * plot_height)
+                canvas.get_tk_widget().configure(width=plot_width,
+                                                 height=plot_height)
+                cue_update_job()
 
     left_button.configure(command=on_left_button)
     right_button.configure(command=on_right_button)
     play_button.configure(command=lambda: on_play_button(1))
-    play_slow_button.configure(command=lambda: on_play_button(0.2))
+    play_slow_button.configure(command=lambda: on_play_button(SLOW_SPEED))
     stop_button.configure(command=on_stop_button)
     select_all_button.configure(command=on_select_all_button)
     jump_to_button.configure(command=on_jump_to_button)
+    plot_delta_button.configure(
+        command=lambda: cue_update_job(redraw_figure=False))
+    hide_rois_button.configure(
+        command=lambda: cue_update_job(redraw_figure=False))
+    spatial_blur_slider.configure(
+        command=lambda: cue_update_job(redraw_figure=False))
+    temporal_blur_slider.configure(
+        command=lambda: cue_update_job(redraw_figure=False))
     tk_image.bind('<Button>', on_image_click)
     for b in plot_buttons:
         b.configure(command=on_plot_setting_button)
